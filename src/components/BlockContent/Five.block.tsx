f1aa665eb37038056ba044aac3b6eb421b0c74d4
tinhtv171295 <130422440+tinhtv171295@users.noreply.github.com>
2025-03-01 14:14:52 +0700

    update corevalue

--- src/components/BlockContent/Five.block.tsx
+++ src/components/BlockContent/Five.block.tsx
@@ -1,31 +1,4 @@
 import styled from "styled-components";
-<<<<<<< HEAD
-import CardInfo from "../Card/CardInfo";
-import { IBlock } from "../../mocks/type";
-import TitleMain from "../Titles/Main.title";
-import { ContainerStl } from "../../Styles/Container";
-
-export default function BlockContentFive({
-  title,
-  titleBody,
-  linkDetail,
-  dataContent,
-  backgroundColor,
-}: IBlock) {
-  return (
-    <Stl.Wrap $bgColor={backgroundColor}>
-      <ContainerStl>
-        <TitleMain
-          title={title}
-          titleBody={titleBody}
-          linkDetail={linkDetail}
-        />
-
-        <Stl.WrapCard>
-          {dataContent.length > 0 &&
-            dataContent.map((item, i) => <CardInfo key={i} data={item} />)}
-        </Stl.WrapCard>
-=======
 import { IBlock } from "../../mocks/type";
 import { ContainerStl } from "../../Styles/Container";
 
@@ -45,26 +18,12 @@ export default function BlockContentFive({ content }: IBlock) {
             )}
           </Stl.Content>
         </Stl.Main>
->>>>>>> feature/corevalue
       </ContainerStl>
     </Stl.Wrap>
   );
 }
 
 const Stl = {
-<<<<<<< HEAD
-  Wrap: styled.div<{ $bgColor?: string }>`
-    padding: 60px 0;
-    ${({ $bgColor }) =>
-      $bgColor !== undefined ? `background-color:${$bgColor}` : ""}
-  `,
-  WrapCard: styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    justify-content: space-between;
-    gap: 30px;
-  `,
-=======
   Wrap: styled.div``,
   Main: styled.div`
     display: flex;
@@ -76,5 +35,4 @@ const Stl = {
     text-align: center;
   `,
   BodyContent: styled.div``,
->>>>>>> feature/corevalue
 };
