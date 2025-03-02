2db1284f56c7596ccbbcaddbaaaa960d186b0171
hoangtd1706 <hoangtd1706@gmail.com>
2025-03-02 17:39:06 +0700

    change style banner and six block

--- src/mocks/banner.data.ts
+++ src/mocks/banner.data.ts
@@ -4,6 +4,7 @@ const bannerData: IBlock[] = [
   {
     type: "banner-fixed",
     title: "Hoạt động sự kiện",
+    titleBody: "Liên hệ chúng tôi",
     backgroundUrl:
       "https://dlsinc.com/storage/setting/1.V77F05e81dd6000098.jpg",
     dataContent: [],
@@ -11,6 +12,7 @@ const bannerData: IBlock[] = [
   {
     type: "banner-scroll",
     title: "Chính sách phúc lợi",
+    titleBody: "Liên hệ chúng tôi",
     backgroundUrl:
       "https://dlsinc.com/storage/setting/1.V77F05e81dd6000098.jpg",
     dataContent: [],
